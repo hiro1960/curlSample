e737fae86835ba767ddb61fb3050ad3014542625
hiro1960 <hiro.maeda.com@gmail.com>
2015-02-23 17:49:24 +0900

    Initial Commit

---
+++ curlSample/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  curlSample
+//
+//  Created by hiro on 2015/02/23.
+//  Copyright (c) 2015年 hiro.maeda. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
